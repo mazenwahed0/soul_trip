6c5d97af01fbdd542f4d2cc6f42088b5258d4f40
mazenwahed0 <mazenprince0@gmail.com>
2025-12-08 18:32:43 +0200

    Update layout_constants.dart

--- lib/features/layout/data/helper/layout_constants.dart
+++ lib/features/layout/data/helper/layout_constants.dart
@@ -41,7 +41,7 @@ abstract class LayoutConstants {
       route: Routes.wishlistView,
     ),
     NavigationItemModel(
-      svgPath: "assets/icons/Community.svg",
+      svgPath: "assets/icons/community.svg",
       label: 'Reviews',
       route: Routes.reviewsView,
     ),
