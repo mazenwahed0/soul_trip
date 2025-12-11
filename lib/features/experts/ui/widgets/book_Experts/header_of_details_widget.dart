ebf42b31d57ba6767521a8c792b55cf984484e46
MinaYasser100 <minayasser43@gmail.com>
2025-12-11 16:13:47 +0200

    edit in the header

--- lib/features/experts/ui/widgets/book_Experts/header_of_details_widget.dart
+++ lib/features/experts/ui/widgets/book_Experts/header_of_details_widget.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:soul_trip/core/theme/colors.dart';
 import 'package:soul_trip/core/theme/soultrip_icons.dart';
@@ -9,40 +8,60 @@ class HeaderOfDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 44),
+    return Container(
+      padding: const EdgeInsets.only(left: 16, right: 16, top: 44, bottom: 16),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-       
+          // Back Button
           Container(
-            width: 50,
-            height: 50,
+            width: 48,
+            height: 48,
             decoration: BoxDecoration(
               color: ColorTheme().grayVeryLight,
               shape: BoxShape.circle,
             ),
             child: IconButton(
               padding: EdgeInsets.zero,
-              iconSize: 28,
-              icon: const Icon(Icons.arrow_back),
+              icon: Icon(
+                Icons.arrow_back_ios_new_rounded,
+                color: ColorTheme().primaryBlue,
+                size: 25,
+              ),
               onPressed: () {
                 Navigator.pop(context);
               },
             ),
           ),
 
-          Text("Expert Details", style: AppTextStyles.semiBold20()),
+          // Title
+          Text(
+            "Expert Details",
+            style: AppTextStyles.semiBold20().copyWith(
+              color: ColorTheme().primaryBlue,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
 
-         
+          // Chat Button
           Container(
-            width: 50,
-            height: 50,
+            width: 48,
+            height: 48,
             decoration: BoxDecoration(
               color: ColorTheme().grayVeryLight,
               shape: BoxShape.circle,
             ),
-            child: const Center(child: Icon(Soultrip.chatRounded, size: 28)),
+            child: IconButton(
+              padding: EdgeInsets.zero,
+              icon: Icon(
+                Soultrip.chatRounded,
+                color: ColorTheme().primaryBlue,
+                size: 25,
+              ),
+              onPressed: () {
+                // TODO: Handle chat button press
+              },
+            ),
           ),
         ],
       ),
